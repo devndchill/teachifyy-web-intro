57d47a8c463f06e8335083807873f10d780eb9bf
kambariyarahull <rahulkambariya.000@gmail.com>
2026-04-29 15:40:51 +0530

    change color

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -23,7 +23,7 @@ export default function CoursesPage() {
         <div className="container-custom">
           <div className="mb-10 text-left">
             <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-2 tracking-tight">
-              Explore <span className="text-primary">Our Courses</span>
+              Explore <span className="text-[#fe4465]">Our Courses</span>
             </h2>
             <p className="text-gray-600 font-medium">
               Comprehensive courses tailored for every career stage.
