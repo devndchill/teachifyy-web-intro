b8c357bcaf6b3427656b485753861392d54359e5
dhavalkaraniya <karaniyadhaval68289@gmail.com>
2026-05-02 14:43:24 +0530

    implemented Facebook Pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { constructMetadata } from "@/lib/metadata";
 import QueryProvider from "@/components/providers/QueryProvider";
 
 const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_ID;
+const FB_PIXEL_ID = "965225332895689";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -42,6 +43,29 @@ export default function RootLayout({
             </Script>
           </>
         )}
+        {/* Facebook Pixel Code */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}
+            (window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${FB_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
+        {/* End Facebook Pixel Code */}
         <QueryProvider>
           <Header />
           <main className="flex-1">{children}</main>
